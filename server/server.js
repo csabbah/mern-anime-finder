d01956ea18e6ee0c23e3afd2af9205bb285130c5
csabbah <csabbah@ryerson.ca>
2022-11-05 21:10:38 -0400

    Updated path in server.js

--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   // Serve up static assets
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.resolve(__dirname, "client", "build")));
   }
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
   db.once("open", () => {
